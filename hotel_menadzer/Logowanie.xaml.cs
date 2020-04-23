8a604f11bd93be148fa9818b1f2fa28e6e3e8abc
Tomek Fus <fustomek38@gmail.com>
2020-04-23 14:35:29 +0200

    pierwszy commit

---
+++ hotel_menadzer/Logowanie.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace hotel_menadzer
+{
+    /// <summary>
+    /// Interaction logic for Logowanie.xaml
+    /// </summary>
+    public partial class Logowanie : Window
+    {
+        public Logowanie()
+        {
+            InitializeComponent();
+        }
+
+        pracownicy O_Pracownik = new pracownicy(); //obiekt klasy pracownicy
+
+        private void Bt_NPr_Click(object sender, RoutedEventArgs e)
+        {
+            NowyPracownik Win_NPr = new NowyPracownik();
+            Win_NPr.ShowDialog();       //ShowDialog blokuje pierwsze okno
+        }
+
+        
+
+        private void Bt_Zaloguj_Click(object sender, RoutedEventArgs e)
+        {
+            
+            
+
+            using (HotelEntities db=new HotelEntities())
+            {
+                if(db.Database.SqlQuery<string>("Select login from pracownicy where login='" + TxB_Login.Text + "'").FirstOrDefault()==TxB_Login.Text)
+                {
+                    LbLoginBlad.Visibility = Visibility.Collapsed;
+                    if(db.Database.SqlQuery<string>("Select haslo from pracownicy where login='" + TxB_Login.Text + "'").FirstOrDefault() == PassB_Haslo.Password)
+                    {
+                        LbHaloBlad.Visibility = Visibility.Collapsed;
+                        MessageBox.Show("Zalogowano");
+                        MainWindow mainWindow = new MainWindow();
+                        mainWindow.Show();
+                        Close();
+                    }
+                    else
+                    {
+                        LbHaloBlad.Content = "Błędne hasło";
+                        LbHaloBlad.Visibility = Visibility.Visible;
+                            
+
+                    }
+                }
+                else
+                {
+                    LbLoginBlad.Content = "Podany login jest błędny lub nie istnieje";
+                    LbLoginBlad.Visibility = Visibility.Visible;
+                }
+                //db.Database.SqlQuery<string>("Select login from pracownicy where login='"+TxB_Login.Text+"'").FirstOrDefault();
+                
+
+
+
+
+
+                //var dataset = from b in db.pracownicy where b.login.StartsWith("a") select b;
+
+                //var dane = db.pracownicy.Where(b => b.login == "adab").FirstOrDefault();
+               // TxB_Login.Text = dane.ToString();
+                
+            }
+            //MessageBox.Show(pracownik_test);
+        }
+    }
+}
---
+++ hotel_menadzer/NowyPracownik.xaml.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace hotel_menadzer
+{
+    /// <summary>
+    /// Interaction logic for NowyPracownik.xaml
+    /// </summary>
+    public partial class NowyPracownik : Window
+    {
+        pracownicy ObPracownik = new pracownicy(); //obeikt clasy pracownicy z DBModel
+
+        
+        void Wyczysc()
+        {
+            TxB_ImiePr.Text = TxB_NazwiskoPr.Text=TxB_Login.Text=PassB_Kod.Password=PassB_Haslo.Password=PassB_PowHaslo.Password="";
+
+        }
+
+
+        public NowyPracownik()
+        {
+            InitializeComponent();
+        }
+
+
+        private string TmpHaslo;
+        private bool SprImie = false;
+        private bool SprNazw = false;
+        private bool SprKPr = false;
+        private bool SprHaslo = false;
+        private bool SprHasloPow = false;
+
+    //imie
+        private void TxB_ImiePr_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(TxB_ImiePr.Text))
+            {
+                LbImieBlad.Visibility = Visibility.Visible;
+                LbImieBlad.Content = "Padaj imie";
+                SprImie = false;
+
+            }
+            else
+            {
+                LbImieBlad.Visibility = Visibility.Collapsed;
+                ObPracownik.imie = TxB_ImiePr.Text.Trim();  //trim usuwa biale znaki przed i po stringu
+                SprImie = true;
+            }
+        }
+
+       
+    //nazwisko
+        private void TxB_NazwiskoPr_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(TxB_NazwiskoPr.Text))
+            {
+                LbNazwiskoBlad.Visibility = Visibility.Visible;
+                LbNazwiskoBlad.Content = "Podaj nazwisko";
+                SprNazw = false;
+            }
+            else
+            {
+                LbNazwiskoBlad.Visibility = Visibility.Collapsed;
+                ObPracownik.nazwisko = TxB_NazwiskoPr.Text.Trim();
+
+                if(SprImie==true)
+                {
+                    ObPracownik.Login();
+                    TxB_Login.Text = ObPracownik.login;
+                }
+
+                
+                SprNazw = true;
+            }
+        }
+
+            
+        
+
+        private void Bt_Utworz_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+
+            //kod
+
+            string _KPr = "gd84pz2";
+            if (String.IsNullOrWhiteSpace(PassB_Kod.Password))
+            {
+                LbKodBlad.Content = "Podaj kod pracodawcy";
+                LbKodBlad.Visibility = Visibility.Visible;
+                SprKPr = false;
+            }
+            else if (PassB_Kod.Password != _KPr)
+            {
+                LbKodBlad.Content = "Błędny kod";
+                LbKodBlad.Visibility = Visibility.Visible;
+                SprKPr = false;
+            }
+            else
+            {
+                LbKodBlad.Visibility = Visibility.Collapsed;
+                SprKPr = true;
+
+            }
+
+
+
+            //haslo
+
+            
+
+
+            if (String.IsNullOrWhiteSpace(PassB_Haslo.Password))
+            {
+                LbHasloBlad.Content = "Podaj hasło";
+                LbHasloBlad.Visibility = Visibility.Visible;
+                SprHaslo = false;
+            }
+            else if (PassB_Haslo.Password.Length < 8)
+            {
+                LbHasloBlad.Content = "Hasło musi mieć min 8 znaków";
+                LbHasloBlad.Visibility = Visibility.Visible;
+                SprHaslo = false;
+            }
+            else
+            {
+                TmpHaslo = PassB_Haslo.Password;
+                LbHasloBlad.Visibility = Visibility.Collapsed;
+                SprHaslo = true;
+            }
+
+
+
+            if (PassB_PowHaslo.Password != TmpHaslo)
+            {
+                LbHasloPowBlad.Content = "Hasło różni się od wpisanego wyżej";
+                LbHasloPowBlad.Visibility = Visibility.Visible;
+                SprHasloPow = false;
+            }
+            else
+            {
+                LbHasloPowBlad.Visibility = Visibility.Collapsed;
+                ObPracownik.haslo = PassB_PowHaslo.Password;
+                SprHasloPow = true;
+            }
+
+
+            if (SprImie == true && SprNazw == true && SprKPr == true && SprHaslo == true && SprHasloPow == true)
+            {
+                using(HotelEntities db=new HotelEntities())
+                {
+                    db.pracownicy.Add(ObPracownik);
+                    db.SaveChanges();
+                }
+                Wyczysc();
+            }
+            else
+            {
+                MessageBox.Show("Popraw dane");
+            }
+        }
+
+        private void Bt_Anuluj_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+
+    }
+}
