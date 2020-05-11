e8925702bf8704065830de28f5e0435b5e161d52
Tomek Fus <fustomek38@gmail.com>
2020-05-11 17:43:07 +0200

    po problemach, dziala

--- hotel_menadzer/MainWindow.xaml.cs
+++ hotel_menadzer/MainWindow.xaml.cs
@@ -20,19 +20,24 @@ namespace hotel_menadzer
     /// </summary>
     public partial class MainWindow : Window
     {
-        
-        
+       
+
+
         public MainWindow()
         {
             InitializeComponent();
             //Bind();
-            
+
         }
 
         DateTime poczatek;
         DateTime koniec;
         TimeSpan lDni; //różnica miedzy dwoma datami
-
+        string l_osob;
+        string nr_p_str;
+        int nr_p_int;
+        
+        
 
         //combobox
         //public List <pokoje> prop_pokoje { get; set; }
@@ -46,7 +51,7 @@ namespace hotel_menadzer
         //}
 
 
-        
+
 
 
         private void Kalendarz_1_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
@@ -78,13 +83,23 @@ namespace hotel_menadzer
                 TxB_DzienDo.Text = date.ToString();
                 Kalendarz_2.Visibility = Visibility.Collapsed;
                 koniec = date;
-                lDni = koniec - poczatek;
-                TxB_LiczbaDni.Text = lDni.Days.ToString();  //róznica miedzy dwoma datami tylko dni
+                if (koniec<poczatek)
+                {
+                    Lb_DataDoBlad.Content = "Błędna data";
+                    Lb_DataDoBlad.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    Lb_DataDoBlad.Visibility = Visibility.Collapsed;
+                    lDni = koniec - poczatek;
+                    TxB_LiczbaDni.Text = lDni.Days.ToString();  //róznica miedzy dwoma datami tylko dni
+                }
+                
             }
         }
 
 
-        
+
 
 
 
@@ -108,12 +123,55 @@ namespace hotel_menadzer
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            
             using (HotelEntities db=new HotelEntities())
             {
-                DataGridP.ItemsSource = db.pokoje.ToList();
+                DataGridP.ItemsSource = db.pokoje.ToList(); //datagrid w zakładce pokoje
+
+                //combobox liczba osob
+                ComBox_LiczbaOs.ItemsSource = db.pokoje.Where(s => s.status == "wolny").ToList();
+            
+            }
+            
+            
+        }
+        //!!!!!!!!!!!!!!!!!!!!!!
+        private void ComBox_LiczbaOs_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            l_osob = this.ComBox_LiczbaOs.SelectedValue.ToString();
+            using (HotelEntities db=new HotelEntities())
+            {
+                //combobox pokoj
+                ComBox_Pokoj.ItemsSource = db.pokoje.Where(s => s.status == "wolny").ToList();
+                ComBox_Pokoj.ItemsSource = db.pokoje.Where(o => o.liczba_osob == l_osob).ToList();
+
             }
         }
 
-        
+        private void ComBox_Pokoj_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            nr_p_str = this.ComBox_Pokoj.SelectedValue.ToString();
+            nr_p_int = int.Parse(nr_p_str);
+
+            using (HotelEntities db=new HotelEntities())
+            {
+                pokoje pokoj = new pokoje();
+                pokoj = db.pokoje.FirstOrDefault(p => p.nr_pokoju == nr_p_int);
+                
+                
+
+            }
+
+        }
+
+        private void RB_Gotowka_Checked(object sender, RoutedEventArgs e)
+        {
+            texttest.Text = "gotówka";
+        }
+
+        private void RB_Karta_Checked(object sender, RoutedEventArgs e)
+        {
+            texttest.Text = "karta";
+        }
     }
 }
